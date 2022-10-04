08b92a37f00f5d0973a6e74001f7f2d37959e7f7
Peter Curtis <peter.curtis@jhuapl.edu>
2022-10-03 20:54:02 -0400

    Make template folder for new days

---
+++ day_template/main.py
@@ -0,0 +1,37 @@
+"""
+<paste in problem description here>
+"""
+
+# === My Solution ===
+
+
+def part1(input: str) -> ...:
+    """
+    """
+    return ...
+
+
+def test_part1():
+    """Check all part 1 examples."""
+    assert part1("") == ...
+
+
+def part2(input: str) -> ...:
+    """
+    """
+    return ...
+
+
+def test_part2():
+    """Check all part 2 examples."""
+    assert part2("") == ...
+
+
+if __name__ == "__main__":
+    problem_input = open('input.txt').read()
+
+    # Print out part 1 solution
+    print(part1(problem_input))
+
+    # Print out part 2 solution
+    print(part2(problem_input))
