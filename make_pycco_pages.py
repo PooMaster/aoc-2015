8aaf598cc045a209488d731e1707982ea56da842
Peter Curtis <peter.curtis@jhuapl.edu>
2022-10-04 17:03:53 -0400

    Try auto building Pycco docs to Pages

---
+++ make_pycco_pages.py
@@ -0,0 +1,50 @@
+import pycco
+from pathlib import Path
+from shutil import rmtree
+import argparse
+import logging
+
+
+def main(exclude):
+    build_dest = Path('./pages')
+    if build_dest.exists():
+        rmtree(build_dest)
+    build_dest.mkdir()
+
+    all_exclude = Path('.gitignore').read_text().splitlines()
+    all_exclude.extend(['.git', '.gitignore', '.github'])
+    all_exclude.extend(exclude)
+
+    source_files = find_files(include=["*.py"], exclude=all_exclude)
+
+    pycco.process(
+        sources=[str(f) for f in source_files],
+        outdir=str(build_dest),
+        index=True
+    )
+
+
+def find_files(path=Path('.'), include=[], exclude=[]):
+    for item in path.iterdir():
+        # Exclude some files and dirs
+        if any(item.match(pattern) for pattern in exclude):
+            continue
+
+        if item.is_dir():
+            # Recursively look for files
+            yield from find_files(item, include, exclude)
+        else:
+            # See if any files match the include patterns
+            if any(item.match(pattern) for pattern in include):
+                yield item
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exclude", help="Comma separated list of file patterns to exclude")
+    args = parser.parse_args()
+
+    exclude = args.exclude.split(',') if args.exclude else []
+    main(exclude)
