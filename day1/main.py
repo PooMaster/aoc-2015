88d0d5c85a5d79d46deffaa3eed146b23c77da64
Peter Curtis <peter.d.curtis@gmail.com>
2022-11-08 20:26:18 -0500

    Update all to new template style

--- day1/main.py
+++ day1/main.py
@@ -1,39 +1,45 @@
 """
+### Part 1:
+
 --- Day 1: Not Quite Lisp ---
 
 Santa was hoping for a white Christmas, but his weather machine's "snow"
 function is powered by stars, and he's fresh out! To save Christmas, he needs
-you to collect fifty stars by December 25th.
+you to collect **fifty stars** by December 25th.
 
 Collect stars by helping Santa solve puzzles. Two puzzles will be made available
 on each day in the Advent calendar; the second puzzle is unlocked when you
-complete the first. Each puzzle grants one star. Good luck!
+complete the first. Each puzzle grants **one star**. Good luck!
 
 Here's an easy puzzle to warm you up.
 
 Santa is trying to deliver presents in a large apartment building, but he can't
 find the right floor - the directions he got are a little confusing. He starts
-on the ground floor (floor 0) and then follows the instructions one character at
+on the ground floor (floor `0`) and then follows the instructions one character at
 a time.
 
-An opening parenthesis, (, means he should go up one floor, and a closing
-parenthesis, ), means he should go down one floor.
+An opening parenthesis, `(`, means he should go up one floor, and a closing
+parenthesis, `)`, means he should go down one floor.
 
 The apartment building is very tall, and the basement is very deep; he will
 never find the top or bottom floors.
 
 For example:
 
-- (()) and ()() both result in floor 0.
-- ((( and (()(()( both result in floor 3.
-- ))((((( also results in floor 3.
-- ()) and ))( both result in floor -1 (the first basement level).
-- ))) and )())()) both result in floor -3.
+- `(())` and `()()` both result in floor `0`.
+
+- `(((` and `(()(()(` both result in floor `3`.
+
+- `))(((((` also results in floor `3`.
+
+- `())` and `))(` both result in floor `-1` (the first basement level).
+
+- `)))` and `)())())` both result in floor `-3`.
 
 To what floor do the instructions take Santa?
 """
 
-# === My Solution ===
+# === Part 1 Solution: ===
 
 from typing import Optional
 
@@ -59,6 +65,26 @@ def test_part1():
     assert part1("))(") == -1
     assert part1(")))") == -3
     assert part1(")())())") == -3
+
+
+"""
+### Part 2:
+
+Now, given the same instructions, find the position of the first character that
+causes him to enter the basement (floor `-1`). The first character in the
+instructions has position `1`, the second character has position `2`, and so on.
+
+For example:
+
+-   `)` causes him to enter the basement at character position `1`.
+
+-   `()())` causes him to enter the basement at character position `5`.
+
+What is the position of the character that causes Santa to first enter the
+basement?
+"""
+
+# === Part 2 Solution: ===
 
 
 def part2(input: str) -> Optional[int]:
@@ -90,10 +116,10 @@ def test_part2():
 
 
 if __name__ == "__main__":
-    problem_input = open('input.txt').read()
+    puzzle_input = open('input.txt').read()
 
     # Print out part 1 solution
-    print(part1(problem_input))
+    print("Part 1:", part1(puzzle_input))
 
     # Print out part 2 solution
-    print(part2(problem_input))
+    print("Part 2:", part2(puzzle_input))
