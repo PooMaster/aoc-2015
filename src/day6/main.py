8f3901d693daaaa54804274c93bd875b5f0d7c00
Peter Curtis <peter.d.curtis@gmail.com>
2022-11-08 20:26:20 -0500

    Completed day 6 part 2

--- src/day6/main.py
+++ src/day6/main.py
@@ -3,20 +3,29 @@
 
 --- Day 6: Probably a Fire Hazard ---
 
-Because your neighbors keep defeating you in the holiday house decorating contest year after year, you've decided to deploy one million lights in a 1000x1000 grid.
+Because your neighbors keep defeating you in the holiday house decorating
+contest year after year, you've decided to deploy one million lights in a
+1000x1000 grid.
 
-Furthermore, because you've been especially nice this year, Santa has mailed you instructions on how to display the ideal lighting configuration.
+Furthermore, because you've been especially nice this year, Santa has mailed you
+instructions on how to display the ideal lighting configuration.
 
-Lights in your grid are numbered from 0 to 999 in each direction; the lights at each corner are at 0,0, 0,999, 999,999, and 999,0. The instructions include whether to turn on, turn off, or toggle various inclusive ranges given as coordinate pairs. Each coordinate pair represents opposite corners of a rectangle, inclusive; a coordinate pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3 square. The lights all start turned off.
+Lights in your grid are numbered from 0 to 999 in each direction; the lights at
+each corner are at `0,0`, `0,999`, `999,999`, and `999,0`. The instructions
+include whether to `turn on`, `turn off`, or `toggle` various inclusive ranges
+given as coordinate pairs. Each coordinate pair represents opposite corners of a
+rectangle, inclusive; a coordinate pair like `0,0 through 2,2` therefore refers
+to 9 lights in a 3x3 square. The lights all start turned off.
 
-To defeat your neighbors this year, all you have to do is set up your lights by doing the instructions Santa sent you in order.
+To defeat your neighbors this year, all you have to do is set up your lights by
+doing the instructions Santa sent you in order.
 """
 
 
 from enum import Enum
 import itertools
 import re
-from typing import Iterable, NamedTuple
+from typing import Iterable, NamedTuple, TypeVar
 
 
 class Pos(NamedTuple):
@@ -28,10 +37,12 @@ class Pos(NamedTuple):
     y: int
 
 
-Grid = dict[Pos, bool]
+# Grids are a variable type to support using bools in part 1 and ints int part 2.
+T = TypeVar("T")
+Grid = dict[Pos, T]
 
 
-def new_grid(size: tuple[int, int], initial_state: bool) -> Grid:
+def new_grid(size: tuple[int, int], initial_state: T) -> Grid[T]:
     """
     Makes and returns a new grid with the given size with all positions in the
     same initial state.
@@ -46,7 +57,6 @@ def new_grid(size: tuple[int, int], initial_state: bool) -> Grid:
 def test_part1() -> None:
     """For example:"""
     lights = new_grid((1000, 1000), False)
-
     assert all(not lit for lit in lights.values())
 
     # > `turn on 0,0 through 999,999` would turn on (or leave on) every light.
@@ -73,7 +83,7 @@ def test_part1() -> None:
 
 
 """
-After following the instructions, how many lights are lit?
+After following the instructions, **how many lights are lit**?
 """
 
 # === Part 1 Solution: ===
@@ -120,7 +130,7 @@ def parse_instruction(instruction_string: str) -> Instruction:
     )
 
 
-def perform_instruction(instruction_string: str, grid: Grid) -> None:
+def perform_instruction(instruction_string: str, grid: Grid[bool]) -> None:
     """
     Follow the instruction to modify the grid in place.
     """
@@ -162,26 +172,92 @@ def part1(input: str) -> int:
 """
 ### Part 2:
 
-<paste in problem description here>
+--- Part Two ---
+
+You just finish implementing your winning light pattern when you realize you
+mistranslated Santa's message from Ancient Nordic Elvish.
+
+The light grid you bought actually has individual brightness controls; each
+light can have a brightness of zero or more. The lights all start at zero.
+
+The phrase `turn on` actually means that you should increase the brightness of
+those lights by `1`.
+
+The phrase `turn off` actually means that you should decrease the brightness of
+those lights by `1`, to a minimum of zero.
+
+The phrase `toggle` actually means that you should increase the brightness of
+those lights by `2`.
+
+What is the total brightness of all lights combined after following Santa's
+instructions?
 """
 
 
 def test_part2() -> None:
     """For example:"""
-    # > `""` results in  `...`.
-    assert part2("") == ...
+    lights = new_grid((1000, 1000), 0)
+    assert all(brightness == 0 for brightness in lights.values())
 
+    # > `turn on 0,0 through 0,0` would increase the total brightness by `1`.
+    perform_instruction2("turn on 0,0 through 0,0", lights)
+    assert lights[Pos(0, 0)] == 1
+    assert total_brightness(lights) == 1
 
-"""
-<end of problem description>
-"""
+    # > `toggle 0,0 through 999,999` would increase the total brightness by `2000000`.
+    perform_instruction2("toggle 0,0 through 999,999", lights)
+    assert total_brightness(lights) == 2_000_001
+
 
 # === Part 2 Solution: ===
 
 
+def perform_instruction2(instruction_string: str, grid: Grid[int]) -> None:
+    """
+    Follow the instruction to modify the grid in place, except according to the
+    part 2 rules this time.
+    """
+    instruction = parse_instruction(instruction_string)
+
+    positions = map(
+        Pos._make, itertools.product(instruction.x_range, instruction.y_range)
+    )
+
+    match instruction.kind:
+        case InstructionKind.on:
+            for pos in positions:
+                grid[pos] += 1
+
+        case InstructionKind.off:
+            for pos in positions:
+                if grid[pos] > 0:
+                    grid[pos] -= 1
+
+        case InstructionKind.toggle:
+            for pos in positions:
+                grid[pos] += 2
+
+
+def total_brightness(lights: Grid[int]) -> int:
+    """
+    Returns the total brightness for a grid of lights.
+    """
+    return sum(lights.values())
+
+
 def part2(input: str) -> ...:
-    """ """
-    return ...
+    """
+    Perform each line of input as an instruction on a 1000 by 1000 grid of
+    lights and return the number of lights lits at the end.
+    """
+
+    lights = new_grid((1000, 1000), 0)
+    instructions = input.splitlines()
+
+    for instruction in instructions:
+        perform_instruction2(instruction, lights)
+
+    return total_brightness(lights)
 
 
 if __name__ == "__main__":
